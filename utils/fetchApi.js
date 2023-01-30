fa9534a88c93335d02311d8bf591f62bc2a5d01a
DevOfHockey <liam.conkin@gmail.com>
2023-01-30 08:37:17 -0800

    Updating npm

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,19 +1,16 @@
 import axios from "axios";
-
 export const baseUrl = 'https://bayut.p.rapidapi.com'
-
 // headers: {
 //     'X-RapidAPI-Key': '94637e7059mshd61b57766804045p141b41jsn3ae497a8a2fd',
 //     'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
 //   }
-
 export const fetchApi = async (url) => {
-    const { data }= await axios.get((url), {
+    const { data } = await axios.get((url), {
         headers: {
             'X-RapidAPI-Key': '94637e7059mshd61b57766804045p141b41jsn3ae497a8a2fd',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     });
-
     return data;
 }
+
