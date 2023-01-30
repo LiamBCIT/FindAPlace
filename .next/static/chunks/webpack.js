08564515f57210e2021b4346c1fdfaa3d25f26d5
DevOfHockey <liam.conkin@gmail.com>
2023-01-29 23:14:40 -0800

    Second Commit - finished property sort pages by rent and mortgage, view individual property pages and sorting system

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6d27eb6855f681e8"; }
+/******/ 		__webpack_require__.h = function() { return "1d05821af5b2be21"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+const footer = () => (
+    <Box textAlign="center" p="5" borderTop="1px" color="gray.600" borderColor="gray.100">
+        2023 FindAPlace, Inc.
+    </Box>
+)
+
+export default footer;
